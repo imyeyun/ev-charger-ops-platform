ac63af0fca0e802f24873696cd265782507dd777
kevincms <kevincms314@naver.com>
2026-02-02 15:04:04 +0900

    localhost 기능 삭제 후 url로 고정

--- nextjs/src/app/api/url.jsx
+++ nextjs/src/app/api/url.jsx
@@ -1,11 +1,3 @@
-import { headers } from "next/headers";
-
-let BACKEND_BASE = "http://localhost:8080";
-
-const host = headers().get("host"); // 예: aivle-test.duckdns.org
-
-if (host && host !== "localhost" && !host.startsWith("127.0.0.1")) {
-  BACKEND_BASE = "http://aivle-test.duckdns.org/spring-api";
-}
+let BACKEND_BASE = "http://aivle-test.duckdns.org/spring-api";
 
 export { BACKEND_BASE };
